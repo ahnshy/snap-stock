14124341b008d7929e3f03e3becc008290f12ec9
ahnshy <ahnshy@naver.com>
2025-05-06 18:18:34 +0900

    <feat> : Adding a plug URI server API

---
+++ app/api/todos/[slug]/route.tsx
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getTodo, deleteTodo, updateTodo } from "@/data/firestore";
+
+// get single item by id
+export async  function GET(req: NextRequest,
+                           { params }: { params: {slug: string} }) {
+
+  console.log(params.slug);
+  if (params.slug === null || params.slug === undefined)
+    return new Response("",{ status : 200 });
+
+  const getedTodo = await getTodo(params.slug);
+
+  if (getedTodo === null) {
+    return new Response("",{ status : 204 });
+  }
+
+  const res = {
+    message: 'get single item by id succeed!',
+    data: getedTodo
+  }
+  return NextResponse.json(res, { status: 200 });
+}
+
+// delete single item by id
+export async  function DELETE(req: Request,
+                              { params }: { params: {slug: string} }) {
+
+  const deletedTodo = await deleteTodo(params.slug);
+
+  if (deletedTodo === null) {
+    return new Response(null,{ status : 204 });
+  }
+
+  const res = {
+    message: 'delete single item by id succeed!'
+  }
+  return NextResponse.json(res, { status: 200 });
+}
+
+// modify single item by id
+export async  function POST(req: Request,
+                            { params }: { params: {slug: string} }) {
+
+  const { title, is_done } = await req.json();
+
+  // const editTodo = {
+  //   id: params.slug,
+  //   title,
+  //   is_done
+  // }
+
+  const updatedTodo = await updateTodo(params.slug, { title, is_done });
+
+  if (updatedTodo === null) {
+    return new Response(null,{ status : 204 });
+  }
+
+  const res = {
+    message: 'modify single item by id succeed!',
+    data: updatedTodo
+  }
+  return NextResponse.json(res, { status: 200 });
+}
