e6c49a8837495d61b24127924ceaf19153ace442
ahnshy <ahnshy@naver.com>
2025-05-18 22:23:27 +0900

    <fix> : Next build error related improvement processing

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,28 +1,9 @@
 // app/api/auth/[...nextauth]/route.tsx
+export const runtime = "nodejs";
 
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
-// 1) NextAuth 옵션 정의
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            authorization: {
-                params: {
-                    scope: "openid email profile",
-                },
-            },
-        }),
-    ],
-    callbacks: {
-        async session({ session }) {
-            return session;
-        },
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "../../../lib/auth"
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
